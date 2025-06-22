35d033dc47b3392b5e61080a9aedecaf9a605204
ArtemLevVv <arzona.art@gmail.com>
2025-06-22 19:32:52 +0200

    worl on edit and delet publications

--- Chat/my_publication/urls.py
+++ Chat/my_publication/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
 
-from .views import ViewPublication, ViewFormPublication
+from .views import ViewPublication, ViewFormPublication, ViewDeletePost, ViewEditPost
 
 urlpatterns = [
-    path('publication/', view=ViewPublication.as_view(), name='my_publication'),
-    path('publication_form/', view= ViewFormPublication.as_view(), name= 'publication_form'),
+    path(route= 'publication/', view=ViewPublication.as_view(), name='my_publication'),
+    path(route= 'publication/<int:pk>/delete', view = ViewDeletePost.as_view(), name= 'delete_post'),
+    path(route= 'publication/<int:pk>/edit', view= ViewEditPost.as_view(), name= 'edit_post'),
+    path(route= 'publication_form/', view= ViewFormPublication.as_view(), name= 'publication_form'),
 ]
--- Chat/my_publication/views.py
+++ Chat/my_publication/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic.base import TemplateView
 from django.views.generic.list import ListView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic import DeleteView
 from django.urls import reverse_lazy
 from .forms import UserPostForm
 from .models import UserPostModel
@@ -33,3 +35,25 @@ class ViewFormPublication(CreateView):
         context = super().get_context_data(**kwargs)
         context['form'] = self.form_class
         return context
+    
+class ViewDeletePost(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = UserPostModel
+    template_name = 'my_publication/confirm_delete.html'  # Можеш створити шаблон
+    success_url = reverse_lazy('my_publication')  # Куди перенаправити після видалення
+
+    def test_func(self):
+        return self.get_object().user == self.request.user
+    
+class ViewEditPost(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = UserPostModel
+    form_class = UserPostForm
+    template_name = 'form_publication/form_publication.html'
+    success_url = reverse_lazy('my_publication')
+
+    def test_func(self):
+        return self.get_object().user == self.request.user
+    
+    def form_valid(self, form):
+        # Переконайся, що автор не змінюється (зазвичай не треба, але на всяк випадок)
+        form.instance.user = self.request.user
+        return super().form_valid(form)
